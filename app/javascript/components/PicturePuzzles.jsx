b58a8cbe6b6134ec1ba8eac019d5f6013782e19a
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-05-29 09:56:58 +0100

    Link individual Puzzles to root and add route using 'dynamic segments'

--- app/javascript/components/PicturePuzzles.jsx
+++ app/javascript/components/PicturePuzzles.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+//This object serves as model, latter the data will be retrieved from the database with only querying the required information (id, title, image source?)
+const picturePuzzles = [{
+  id: 1,
+  title: "The Smurfs",
+  imageSrc: "XXX.png",
+  coordinatesTargets: [],
+  taskDescription: "Find Papa Smurf, Smurfette and Brainy Smurf."
+}, {
+  id: 2,
+  title: "Forest Life",
+  imageSrc: "XXX.png",
+  coordinatesTargets: [],
+  taskDescription: "Find all Ladybugs."
+}, {
+  id: 3,
+  title: "Ocean World",
+  imageSrc: "XXX.png",
+  coordinatesTargets: [],
+  taskDescription: "Find all Hammerhead sharks."
+}]
 
 function PicturePuzzles() {
+  const puzzles = picturePuzzles.map((picturePuzzle) => <Link to={`/${picturePuzzle.id}`} key={picturePuzzle.id}>{picturePuzzle.title}<br></br></Link>)
   return(
-    <h1>Choose a Picture Puzzle from below 3:</h1>
+    <> 
+    <h1>Choose a Puzzle</h1>
+    {puzzles}
+    </>
+   
   )
 }
 
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,9 +1,13 @@
 import PicturePuzzles from "../components/PicturePuzzles";
+import PicturePuzzle from "../components/PicturePuzzle"
 import React from "react";
 
 const routes = [{
   path: "/",
   element: <PicturePuzzles />
+}, {
+  path: "/:id",
+  element: <PicturePuzzle />
 }]
 
 export default routes;
