3d284ce9b97c870ca40dcdbcb8ac75419704cc1d
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-02 17:15:52 +0100

    Add data for 'The Smurfs' to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puzzle = PicturePuzzle.find_or_initialize_by(title: "The Smurfs")
+
+puzzle.assign_attributes(
+  image_src: "/picture-puzzle-images/the-smurfs.jpg",
+  task_description: "Find Papa Smurf, Smurfette and Brainy Smurf.",
+  resolution_width: 1000,
+  resolution_height: 674,
+  targets: [
+    { name: "Smurfette", boundingBox: { xMin: 90, xMax: 140, yMin: 360, yMax: 440 } },
+    { name: "Papa Smurf", boundingBox: { xMin: 410, xMax: 475, yMin: 420, yMax: 507 } },
+    { name: "Brainy Smurf", boundingBox: { xMin: 277, xMax: 343, yMin: 525, yMax: 655 } }
+  ]
+)
+
+puzzle.save!
