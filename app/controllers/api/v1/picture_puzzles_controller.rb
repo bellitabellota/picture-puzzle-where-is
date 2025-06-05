02ac37dcbaafed638354939d98bcd02bc646cb49
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-05 14:58:55 +0100

    Set player_id in session for all requests going through PicturePuzzleController

--- app/controllers/api/v1/picture_puzzles_controller.rb
+++ app/controllers/api/v1/picture_puzzles_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PicturePuzzlesController < ApplicationController
+  before_action :set_player_id
+
   def index
     @puzzles = PicturePuzzle.all
     render json: @puzzles
@@ -7,5 +9,11 @@ class Api::V1::PicturePuzzlesController < ApplicationController
   def show
     @puzzle = PicturePuzzle.find(params[:id])
     render json: @puzzle
+  end
+
+  private
+
+  def set_player_id
+    session[:player_id] = session[:player_id] || SecureRandom.uuid
   end
 end
