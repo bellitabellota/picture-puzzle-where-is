bf6a155645283871af8e8b8ff5e5439da814e95f
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-05 15:00:29 +0100

    Add PuzzleTimerController.start_timer to track start of the game

---
+++ app/controllers/api/v1/puzzle_timers_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::PuzzleTimersController < ApplicationController
+  def start_timer
+    @puzzle_timer = PuzzleTimer.find_or_create_by(
+      picture_puzzle_id: params[:id],
+      player_id_session: session[:player_id]
+    )
+
+    if @puzzle_timer.update(start_time: Time.current) # Time.current gets the UTC time
+      render json: { message: "Timer started", start_time: @puzzle_timer.start_time }, status: :ok
+    else
+      render json: { error: "Failed to start timer" }, status: :unprocessable_entity
+    end
+  end
+end
