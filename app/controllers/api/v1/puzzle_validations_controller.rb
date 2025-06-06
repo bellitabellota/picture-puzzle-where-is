4a6856e9ec19e3ee25747d2a7fd3600aaf2a50fd
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-06 15:39:36 +0100

    Expand validate_guess saving correctly identified targets and end_time in the puzzle_timers db

--- app/controllers/api/v1/puzzle_validations_controller.rb
+++ app/controllers/api/v1/puzzle_validations_controller.rb
@@ -1,10 +1,27 @@
 class Api::V1::PuzzleValidationsController < ApplicationController
   def validate_guess
     puzzle = PicturePuzzle.find(params[:id])
+    puzzle_timer = PuzzleTimer.find_by(picture_puzzle_id: params[:id], player_id_session: session[:player_id])
 
     clicked_target = get_target_of_clicked_coordinates(params[:originalX], params[:originalY], puzzle.targets)
 
     if clicked_target && (params[:selectedName] == clicked_target["name"])
+      unless puzzle_timer.identified_targets.include?(clicked_target["name"])
+        target_saved = puzzle_timer.update(identified_targets: puzzle_timer.identified_targets.push(clicked_target["name"]))
+
+        unless target_saved
+          return render json: { error: "Failed to save identified target in puzzle timer" }, status: :unprocessable_entity
+        end
+      end
+
+      if puzzle_timer.identified_targets.sort == puzzle.targets.map { |target| target["name"] }.sort
+        end_time_saved = puzzle_timer.update(end_time: Time.current)
+
+        unless end_time_saved
+          return render json: { error: "Failed to save end time in puzzle timer" }, status: :unprocessable_entity
+        end
+      end
+
       x_center = getMedian(clicked_target["boundingBox"]["xMin"], clicked_target["boundingBox"]["xMax"])
       y_center = getMedian(clicked_target["boundingBox"]["yMin"], clicked_target["boundingBox"]["yMax"])
 
