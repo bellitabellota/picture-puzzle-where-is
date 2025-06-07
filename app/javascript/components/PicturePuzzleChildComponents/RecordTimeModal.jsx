dda0cc392a0260190864fe49212c296910ae1442
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-07 10:48:11 +0100

    Format time displaying in RecordTimeModal

--- app/javascript/components/PicturePuzzleChildComponents/RecordTimeModal.jsx
+++ app/javascript/components/PicturePuzzleChildComponents/RecordTimeModal.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 function RecordTimeModal({secondsToCompletion}) {
+  const minutes = Math.floor(secondsToCompletion / 60);
+  const remainingSeconds = secondsToCompletion % 60;
+
   return(
     <div className="modal">
       <h2>PUZZLE FINISHED</h2>
-      <p>You solved the puzzle in {secondsToCompletion} seconds.</p>
+      <p>You solved the puzzle in {minutes !== 0 && (String(minutes) + "minute(s) and ")} {remainingSeconds} seconds.</p>
 
 
         <label for="player-name">Enter your name (if you want your time to be recorded).</label>
