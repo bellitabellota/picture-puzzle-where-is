8e56223ad9ed5b80a5eed02cc3a155e915942555
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-08 15:20:35 +0100

    Display fetched data in PuzzleResults component and add CSS

--- app/javascript/components/PicturePuzzleChildComponents/PuzzleResults.jsx
+++ app/javascript/components/PicturePuzzleChildComponents/PuzzleResults.jsx
@@ -1,35 +1,57 @@
 import React, {useEffect, useState} from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function PuzzleResults() {
   const params = useParams();
   const [puzzleResults, setPuzzleResults] = useState([]);
   const [puzzleTitle, setPuzzleTitle] = useState(null);
-  
-  console.log(`puzzleResults ${puzzleResults}`)
-  console.log(puzzleTitle)
-  console.log(puzzleResults[0])
 
   useEffect(() => {
     const url =`/api/v1/picture_puzzles/${params.id}/results`;
-
     fetch(url)
     .then((response) => {
       if(!response.ok) {
         throw new Error("Network response not ok")
       }
       return response.json()
-    }).then((data) => {
-      console.log(data)
-      setPuzzleTitle(data.title)
+    }).then((data) => {console.log(data.puzzleTitle)
+
+      setPuzzleTitle(data.puzzleTitle);
       setPuzzleResults(data.results);
     }).catch((error) => {
       console.log(error);
     })
   }, [])
 
+  const resultElements = puzzleResults.map((result, index) => {
+    const minutes = Math.floor(result.seconds_to_completion/ 60);
+    const remainingSeconds = result.seconds_to_completion % 60;
+
+    return(
+      <div key={result.id}>
+        <p className="rank">{index + 1}</p>
+        <p className="name">{result.name}</p>
+        <p className="duration">{`${minutes} m ${remainingSeconds} s `}</p>
+
+      </div>
+    )
+  })
+
   return (
-    <p>{params.id}</p>
+    <main className="main-picture-puzzle-results">
+
+    <Link to="/" >&lt; Back to Home</Link>
+    <h1>{puzzleTitle} - Results</h1>
+
+    <div className="results-container">
+      <div className="label-container">
+        <p className="rank-label">Rank</p>
+        <p className="name-label">Name</p>
+        <p className="duration-label">Duration</p>
+      </div>
+      { resultElements }
+    </div>
+  </main>
   )
 }
 
