4149ac7b9e172bc15ef5c50d16992f61dc1fa4b6
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-09 14:00:36 +0100

    Add cleanup function

--- app/javascript/components/custom_hooks/usePuzzleResults.js
+++ app/javascript/components/custom_hooks/usePuzzleResults.js
@@ -5,8 +5,11 @@ const usePuzzleResults = (paramsId) => {
   const [puzzleTitle, setPuzzleTitle] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     const url =`/api/v1/picture_puzzles/${paramsId}/results`;
-    fetch(url)
+    fetch(url, {signal})
     .then((response) => {
       if(!response.ok) {
         throw new Error("Network response not ok")
@@ -18,6 +21,8 @@ const usePuzzleResults = (paramsId) => {
     }).catch((error) => {
       console.log(error);
     })
+
+    return () => controller.abort(); // the cleanup function was added because the effect runs when the component mounts. So in case the component unmount before the request completes, the request would have been still active but the component would not be there anymore to handle the response.
   }, [])
 
   return {puzzleResults, puzzleTitle}
