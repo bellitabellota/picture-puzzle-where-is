a9a3359813a6e38010813908c5694158e1539e56
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-09 08:02:35 +0100

    Add length validation for name

--- app/models/puzzle_result.rb
+++ app/models/puzzle_result.rb
@@ -1,6 +1,6 @@
 class PuzzleResult < ApplicationRecord
   belongs_to :picture_puzzle
 
-  validates :name, presence: :true
+  validates :name, presence: :true, length: { maximum: 40 }
   validates :seconds_to_completion, presence: :true
 end
