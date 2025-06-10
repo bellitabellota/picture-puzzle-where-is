5aa269f6f9187157dee7be2d90a596b0b00d5b95
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-10 09:08:20 +0100

    Reset identified targets when resetting start_time

--- app/controllers/api/v1/puzzle_timers_controller.rb
+++ app/controllers/api/v1/puzzle_timers_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::PuzzleTimersController < ApplicationController
       player_id_session: session[:player_id]
     )
 
-    if puzzle_timer.update(start_time: Time.current) # Time.current gets the UTC time
+    if puzzle_timer.update(start_time: Time.current, identified_targets: []) # Time.current gets the UTC time
       render json: { message: "Timer started", start_time: puzzle_timer.start_time }
     else
       render json: { error: "Failed to start timer" }, status: :unprocessable_entity
