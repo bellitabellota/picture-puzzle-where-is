7cccdd233c5bb998250079d5ee48dfce6014dc07
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-11 09:03:59 +0100

    Add CSS for root PicturePuzzles

--- app/javascript/components/PicturePuzzles.jsx
+++ app/javascript/components/PicturePuzzles.jsx
@@ -4,16 +4,29 @@ import usePicturePuzzles from "./custom_hooks/usePicturePuzzles";
 
 function PicturePuzzles() {
   const {picturePuzzles, error, isLoading} = usePicturePuzzles();
+  
 
   if(isLoading) return <p>Puzzles are loading ...</p>
   if(error) return <p>{error.message}</p>;
+  console.log(picturePuzzles[0])
 
-  const puzzles = picturePuzzles.map((picturePuzzle) => <Link to={`/${picturePuzzle.id}`} key={picturePuzzle.id}>{picturePuzzle.title}<br></br></Link>)
+  const puzzles = picturePuzzles.map((picturePuzzle) => 
+    <Link to={`/${picturePuzzle.id}`} >
+      <div className="picture-puzzle" key={picturePuzzle.id}>
+        <div className="image-container">
+          <img src={picturePuzzle.image_src} className="puzzle-image" />
+        </div>
+        <p className="puzzle-title">{picturePuzzle.title}</p>
+      </div>
+    </Link>
+  )
 
   return(
-    <main>
-      <h1>Choose a Puzzle</h1>
-      {puzzles}
+    <main className="main-picture-puzzles">
+      <h1>Choose A Puzzle</h1>
+      <div className="grid-container">
+        {puzzles}
+      </div>
     </main>
   )
 }
