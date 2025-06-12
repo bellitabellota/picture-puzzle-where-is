5c088449c4a9d79c12342c8504c0f764d267b705
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-12 14:57:26 +0100

    Add PicturePuzzle tests for puzzle is loading, puzzle could not be loaded and puzzle is rendered

---
+++ app/javascript/tests/PicturePuzzle.test.jsx
@@ -0,0 +1,74 @@
+import PicturePuzzle from "../components/PicturePuzzle";
+import usePicturePuzzle from "../components/custom_hooks/usePicturePuzzle";
+import {render, screen } from "@testing-library/react";
+import React from "react";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import routes from "../routes/index"
+import useValidateGuess from "../components/custom_hooks/useValidateGuess";
+import useGameState from "../components/custom_hooks/useGameState";
+import useStartTimer from "../components/custom_hooks/useStartTimer";
+import usePuzzleFrontendTimer from "../components/custom_hooks/usePuzzleFrontendTimer";
+
+vi.mock("../components/custom_hooks/usePicturePuzzle", () => ({ default: vi.fn() }))
+vi.mock("../components/custom_hooks/useValidateGuess", () => ({ default: vi.fn() }))
+vi.mock("../components/custom_hooks/useGameState", () => ({ default: vi.fn() }))
+vi.mock("../components/custom_hooks/useStartTimer", () => ({ default: vi.fn() }))
+vi.mock("../components/custom_hooks/usePuzzleFrontendTimer", () => ({ default: vi.fn() }))
+
+const testPuzzle = {
+  id: 123,
+  title: "Fun Test Puzzle",
+  imageSrc: "/picture-puzzle-images/test-puzzle.jpg",
+  taskDescription: "Find all Test Targets.",
+  resolution_width: 1280,
+  resolution_height: 720,
+  targets: [
+      { name: "Test Target 1" },
+      { name: "Test Target 2" },
+      { name: "Test Target 3" }
+  ]
+}
+
+const memoryRouter = createMemoryRouter(routes, { initialEntries: ["/123"] })
+
+describe("PicturePuzzle", ()=> {
+  it("renders 'Puzzle is loading ...' when isLoading is true", () => {
+    usePicturePuzzle.mockReturnValue({puzzle: null, error: null, isLoading: true})
+    useValidateGuess.mockReturnValue({ correctlyIdentifiedTargets: [], validationError: null });
+    useGameState.mockReturnValue({ secondsToCompletion: null, gameStateError: null })
+    useStartTimer.mockReturnValue({ startTimerError: null });
+
+    usePuzzleFrontendTimer.mockReturnValue({ secondsPassed: 0 });
+
+    render(<PicturePuzzle />)
+
+    expect(screen.getByText("Puzzle is loading ...")).toBeInTheDocument();
+  })
+
+  it("renders error message if usePicturePuzzle returned an Error", () => {
+    usePicturePuzzle.mockReturnValue({puzzle: null, error: new Error("server error"), isLoading: false})
+    useValidateGuess.mockReturnValue({ correctlyIdentifiedTargets: [], validationError: null });
+    useGameState.mockReturnValue({ secondsToCompletion: null, gameStateError: null })
+    useStartTimer.mockReturnValue({ startTimerError: null });
+
+    usePuzzleFrontendTimer.mockReturnValue({ secondsPassed: 0 });
+
+    render(<PicturePuzzle />)
+    expect(screen.getByText("server error")).toBeInTheDocument();
+  })
+
+  it("renders puzzle if usePicturePuzzle returned a puzzle", ()=> {
+    usePicturePuzzle.mockReturnValue({ puzzle: testPuzzle, error: false, isLoading: false})
+    useValidateGuess.mockReturnValue({ correctlyIdentifiedTargets: [], validationError: null });
+    useGameState.mockReturnValue({ secondsToCompletion: null, gameStateError: null })
+    useStartTimer.mockReturnValue({ startTimerError: null });
+
+    usePuzzleFrontendTimer.mockReturnValue({ secondsPassed: 0 });
+
+    const {container} = render(
+      <RouterProvider router={memoryRouter}></RouterProvider>
+    )
+
+    expect(container).toMatchSnapshot();
+  })
+});
