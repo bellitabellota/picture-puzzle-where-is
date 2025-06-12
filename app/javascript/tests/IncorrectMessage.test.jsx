7d8cec30a56d12a3ca543efb982f3224822ae4f7
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-12 08:11:43 +0100

    Add tests for IncorrectMessage component

---
+++ app/javascript/tests/IncorrectMessage.test.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {render, screen } from "@testing-library/react";
+import IncorrectMessage from "../components/PicturePuzzleChildComponents/IncorrectMessage";
+
+describe("IncorrectMessage", () => {
+  const testMessage = "Test incorrect message";
+  const mockSetIncorrectMessage = vi.fn();
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  })
+
+  it("renders the given message", () => {
+    render(<IncorrectMessage message={testMessage} setIncorrectMessage={mockSetIncorrectMessage} />)
+
+    expect(screen.getByText(testMessage)).toBeInTheDocument();
+  })
+
+  it("clears message after 2 seconds", () => {
+    render(
+      <IncorrectMessage 
+        message={testMessage} 
+        setIncorrectMessage={mockSetIncorrectMessage} 
+      />
+    );
+
+    expect(screen.getByText(testMessage)).toBeInTheDocument();
+    
+    vi.advanceTimersByTime(2000);
+    
+    expect(mockSetIncorrectMessage).toHaveBeenCalledTimes(1);
+    expect(mockSetIncorrectMessage).toHaveBeenCalledWith(null);
+  })
+
+})
