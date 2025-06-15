aab355888f80701e1f1a4966b71c2446d01d4e79
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-15 16:50:47 +0100

    Add factory for :puzzle_result and :puzzle_timer

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,4 +11,18 @@ FactoryBot.define do
       { name: "Test Object 3", boundingBox: { xMin: 150, xMax: 170, yMin: 160, yMax: 200 } }
     ] }
   end
+
+  factory :puzzle_result do
+    name { Faker::Name.first_name }
+    seconds_to_completion { 127 }
+    association :picture_puzzle, factory: :picture_puzzle
+  end
+
+  factory :puzzle_timer do
+    start_time { "2025-06-15 13:30:39.504725000 +0000" }
+    end_time { "2025-06-14 13:57:35.456780000 +0000" }
+    player_id_session { "e4e48d20-20e3-4da2-90d2-b804f704c733" }
+    association :picture_puzzle, factory: :picture_puzzle
+    identified_targets { [ "Test Object 3" ] }
+  end
 end
