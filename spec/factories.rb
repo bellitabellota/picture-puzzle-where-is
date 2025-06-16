73be4e5873e191493c57bbc8ba4a0d80d9591694
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-06-16 12:35:05 +0100

    Correct coordinates in picture_puzzle factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     resolution_width { 1000 }
     resolution_height { 674 }
     targets { [
-      { name: "Test Object 1", boundingBox: { xMin: 50, xMax: 40, yMin: 80, yMax: 50 } },
-      { name: "Test Object 2", boundingBox: { xMin: 110, xMax: 90, yMin: 140, yMax: 110 } },
+      { name: "Test Object 1", boundingBox: { xMin: 40, xMax: 50, yMin: 50, yMax: 80 } },
+      { name: "Test Object 2", boundingBox: { xMin: 90, xMax: 110, yMin: 110, yMax: 140 } },
       { name: "Test Object 3", boundingBox: { xMin: 150, xMax: 170, yMin: 160, yMax: 200 } }
     ] }
   end
